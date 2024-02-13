9fb46ad939b73b0f895ff1b696b047b5042df9c8
DmytroDiakun0 <diakun.dm@gmail.com>
2024-02-13 21:42:57 +0200

    Add test data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const v1ContractRoutes = require('./v1/routes/contractRoutes')
 const v1CustomerRoutes = require('./v1/routes/customerRoutes')
 const v1VehicleRoutes = require('./v1/routes/vehicleRoutes')
-
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/v1/contracts', v1ContractRoutes);
 app.use('/api/v1/customers', v1CustomerRoutes);
 app.use('/api/v1/vehicles', v1VehicleRoutes);
